899b7d498217c3e84845651648fd0911f877f426
Adam Strickland <adam.strickland@gmail.com>
2011-09-22 18:30:27 -0500

    go first fractal

--- themes/fractal/app.rb
+++ themes/fractal/app.rb
@@ -1,19 +1,24 @@
-# Use the app.rb file to load Ruby code, modify or extend the models, or
-# do whatever else you fancy when the theme is loaded.
-
+# # Use the app.rb file to load Ruby code, modify or extend the models, or
+# # do whatever else you fancy when the theme is loaded.
+# 
+# module Nesta
+#   class App
+#     # Uncomment the Rack::Static line below if your theme has assets
+#     # (i.e images or JavaScript).
+#     #
+#     # Put your assets in themes/fallout/public/fallout.
+#     #
+#     # use Rack::Static, :urls => ["/fallout"], :root => "themes/fallout/public"
+# 
+#     helpers do
+#       # Add new helpers here.
+#     end
+# 
+#     # Add new routes here.
+#   end
+# end
 module Nesta
   class App
-    # Uncomment the Rack::Static line below if your theme has assets
-    # (i.e images or JavaScript).
-    #
-    # Put your assets in themes/fallout/public/fallout.
-    #
-    # use Rack::Static, :urls => ["/fallout"], :root => "themes/fallout/public"
-
-    helpers do
-      # Add new helpers here.
-    end
-
-    # Add new routes here.
+    use Rack::Static, :urls => ['/fractal'], :root => 'themes/fractal/public'
   end
 end
